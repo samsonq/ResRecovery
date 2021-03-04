f86fb3a2a200f6460c45be874fa91c06116f36f7
Samson Qian <samsonqian@gmail.com>
2021-03-03 19:23:36 -0800

    flask

--- app.py
+++ app.py
@@ -1,16 +1,72 @@
-from flask import Flask
-from datetime import datetime
+import os
+import sys
+import numpy as np
+from scipy import stats
+import pandas as pd
+import pickle
+import io
+from io import StringIO
+from flask import Flask, render_template, request, jsonify, make_response
+from aggregate import *
+import warnings
+warnings.filterwarnings("ignore")
+
 app = Flask(__name__)
+baseline_model = pickle.load(open("./model/baseline_model.pkl", "rb"))
+extended_model = pickle.load(open("./model/extended_model.pkl", "rb"))
+chunk_size = 120
+
 
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+def home():
+    return render_template('index.html')
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    """.format(time=the_time)
+
+def transform(text_file_contents):
+    return text_file_contents.replace("=", ",")
+
+
+@app.route('/baseline', methods=["POST"])
+def predict_baseline():
+    f = request.files['data_file']
+    if not f:
+        return "No file"
+
+    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    stream.seek(0)
+    result = transform(stream.read())
+    df = pd.read_csv(StringIO(result))
+
+    # Preprocessing & Feature Building
+    X = create_features([df], chunk_size)
+    X = X[['dwl_bytes_avg', 'dwl_peak_prom', 'upl_bytes_std', 'dwl_bytes_std', 'dwl_max_psd', 'dwl_num_peak']]
+    prediction = stats.mode(extended_model.predict(X))[0][0]
+    resolutions = {1: "240p", 2: "480p", 3: "1080p"}
+
+    return render_template('index.html',
+                           prediction_text_extended='The predicted resolution is: {}'.format(resolutions[prediction]))
+
+
+@app.route('/extended', methods=["POST"])
+def predict_extended():
+    f = request.files['data_file']
+    if not f:
+        return "No file"
+
+    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    stream.seek(0)
+    result = transform(stream.read())
+    df = pd.read_csv(StringIO(result))
+
+    # Preprocessing & Feature Building
+    X = create_features([df], chunk_size)
+    X = X[['dwl_bytes_avg', 'dwl_peak_prom', 'upl_bytes_std', 'dwl_bytes_std', 'dwl_max_psd', 'dwl_num_peak']]
+    prediction = stats.mode(extended_model.predict(X))[0][0]
+    resolutions = {1: "Low", 2: "Medium", 3: "High"}
+
+    return render_template('index.html',
+                           prediction_text_extended='The predicted resolution is: {}'.format(resolutions[prediction]))
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True)
