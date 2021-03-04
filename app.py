8de50367fd492b317183b233f74684dc1431000b
Samson Qian <samsonqian@gmail.com>
2021-03-03 19:07:57 -0800

    flask

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def homepage():
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
